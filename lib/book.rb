b4f69fb7ba654fcb97d8159459c48cd74be9e5a9
Christopher Stull <chris.stull@gmail.com>
2018-08-28 18:05:04 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,7 +2,7 @@ class Book
   
   attr_accessor :title,:author,:page_count,:genre
   
-  def initialize(title,author,page_count,genre)
+  def initialize(title,author=nil,page_count=nil,genre=nil)
     @title = title
     @author = author
     @page_count = page_count
